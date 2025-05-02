54b6ecc4b2409fa9b8fca1faab11836b2b6c195f
Ivan Herrera <71898783+Ivan-Herrera-Garcia@users.noreply.github.com>
2025-05-02 09:19:26 -0600

    mejoras de seo

--- app/layout.js
+++ app/layout.js
@@ -44,35 +44,29 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning="true" data-lt-installed="true">
       <head>
-        <title>Immarketing | Marketing | TI | Legal | Finanzas | Diseño grafico | Audiovisual</title>
+        <title>Immarketing | Marketing, TI, Legal, Finanzas y Diseño</title>
         <meta
           name="description"
-          content="Servicios de marketing, TI, legal, finanzas, diseño gráfico y audiovisual para tu empresa.
-          Imkt te ofrece una amplia gama de servicios para tu empresa. Con servicios exclusivos y personalizados, 
-          somos tu aliado ideal para el crecimiento y éxito de tu negocio. Ubicados en Torreón, Coahuila, México."
+          content="Servicios de marketing, TI, legal, finanzas, diseño y audiovisual. En Imkt potenciamos tu empresa con soluciones personalizadas desde Torreón, México."
         />
         <link rel="icon" href="/logo_svg.svg" />
         <link rel="apple-touch-icon" href="https://immarketing.netlify.app/preview.jpg" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         
-        <meta property="og:title" content="Immarketing | Marketing | TI | Legal | Finanzas | Diseño grafico | Audiovisual" />
+        <meta property="og:title" content="Immarketing | Marketing, TI, Legal, Finanzas y Diseño" />
         <meta
           property="og:description"
-          content="Servicios de marketing, TI, legal, finanzas, diseño gráfico y audiovisual para tu empresa.
-          Imkt te ofrece una amplia gama de servicios para tu empresa. Con servicios exclusivos y personalizados, 
-          somos tu aliado ideal para el crecimiento y éxito de tu negocio. Ubicados en Torreón, Coahuila, México."
+          content="Servicios de marketing, TI, legal, finanzas, diseño y audiovisual. En Imkt potenciamos tu empresa con soluciones personalizadas desde Torreón, México."
         />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://immarketing.netlify.app/" />
         <meta property="og:image" content="https://immarketing.netlify.app/preview.jpg" />
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Immarketing | Marketing | TI | Legal | Finanzas | Diseño grafico | Audiovisual" />
+        <meta name="twitter:title" content="Immarketing | Marketing, TI, Legal, Finanzas y Diseño" />
         <meta
           name="twitter:description"
-          content="Servicios de marketing, TI, legal, finanzas, diseño gráfico y audiovisual para tu empresa.
-          Imkt te ofrece una amplia gama de servicios para tu empresa. Con servicios exclusivos y personalizados, 
-          somos tu aliado ideal para el crecimiento y éxito de tu negocio. Ubicados en Torreón, Coahuila, México."
+          content="Servicios de marketing, TI, legal, finanzas, diseño y audiovisual. En Imkt potenciamos tu empresa con soluciones personalizadas desde Torreón, México."
         />
         <meta name="twitter:image" content="https://immarketing.netlify.app/preview.jpg" />
         <meta name="twitter:image:alt" content="Imkt Logo" />
